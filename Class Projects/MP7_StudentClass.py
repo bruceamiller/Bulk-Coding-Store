660e1a9fb0b927c1bab01ffabd631dfd1b7501d8
bruceamiller <159710432+bruceamiller@users.noreply.github.com>
2024-04-16 12:12:05 -0500

    MP7 Working Draft

---
+++ Class Projects/MP7_StudentClass.py
@@ -0,0 +1,76 @@
+#
+# Student Class Definition
+#
+# 4 Instance Variables:
+# _stuID str - 5 characters, 2 alpha (initials), 3 numeric (random)
+# _name list - 2 str elements, first name, last name
+# _testScores list - int values (could be empty, no tests taken)
+# _avg float - the average of _testScores (0.0 if no tests taken)
+#
+# Constructor:
+# __init__(self, stuID) Initialize _stuId to stuID, _name to a list with
+# 2 empty str elements, _testScores to the empty list,
+# _avg to 0.0
+#
+# __str__ is defined for out class (the str constructor)
+#
+# 6 Public Instance Methods:
+# getID(self) return _stuID
+# getName(self) return _name
+# getTestScores(self) return _testScores
+# getAvg(self) return _avg
+# setName(self, firstName, lastName) Change the 2 elements in _name to
+# firstName and lastName.
+# addTest(self, testScore) Append 1 testScore onto _testScores,
+# then call _calcAvg() to set _avg to
+# the updated value.
+#
+# 1 Private Instance Method:
+# _calcAvg(self) called by addTest() to keep _avg accurate every time
+# a new test score is added, returns a float value that
+# is the average of the test scores, 0.0 if no test scores
+#
+
+class Student:
+
+    def __init__(self, stuID):
+        self._stuID = stuID
+        self._name = ("", "")
+        self._testScores = []
+        self._avg = 0.0
+
+    def __str__(self):
+        scoresText = ""
+        for score in self._testScores:
+            scoresText += str(score) + " "
+        outputStr = f"Student: {self._stuID} {self._name[0]} {self._name[1]}\n"
+        outputStr += f"Test Scores: {scoresText}\n"
+        outputStr += f"Test Average: {self._avg:.2f}"
+        return outputStr
+
+    def getID(self):
+        return self._stuID
+    def getName(self):
+        return self._name
+    def getTestScores(self):
+        return self._testScores
+    def getAvg(self):
+        return self._avg
+    
+    def setName(self, firstName, lastName):
+        newName = (firstName, lastName)
+        self._name = newName
+    def addTest(self, testScore):
+        self._testScores.append(testScore)
+        
+        self._calcAvg()
+
+    def _calcAvg(self):
+        numScores = len(self._testScores)
+        average = 0.0
+        for score in self._testScores:
+            average += score / numScores
+        self._avg = average
+
+
+
---
+++ Class Projects/Machine Problem 7.py
@@ -0,0 +1,135 @@
+from MP7_StudentClass import Student
+
+#
+# Machine Problem 7
+# CS 1300
+# Bruce Miller
+#
+# Description:
+#
+# Get studentd ID, name, testScores, and testAvg
+# Print everything in organized & labeled lines
+#
+
+def getStudents():
+    #
+    # Opens the data file of student info... studentID, firstName, lastName, then
+    # testScores - the number of test scores is variable, from zero up... reads
+    # each line of data as a str, divides the line into the values... str, str, str,
+    # int, int, int... (a variable number of int values - could be none)...
+    # instantiates a new Student object, uses the Student object's instance methods
+    # to add those values to the object, adds the Student object to a dictionary of
+    # Student objects using studentID as the key and the Student object as the value,
+    # and returns the dictionary of Student objects.
+    #
+    # There are no parameters.
+    #
+    # Returns a dict of objects from the Student class, using student ID as the key
+    # and the Student object as the value
+    #
+
+    file = open("C:\\Users\\Bruce\\Desktop\\Python Coding\\Class Projects\\1300 - MP6 Data.txt")
+    lines = file.readlines()
+    file.close()
+    for linePos in range(len(lines)):
+        lines[linePos] = lines[linePos].split()
+
+    roster = {}
+    for studentInfo in lines:
+        currentStudent = Student(studentInfo[0])
+        currentStudent.setName(studentInfo[1], studentInfo[2])
+        for testScore in studentInfo[3:]:
+            currentStudent.addTest(int(testScore))
+        roster[studentInfo[0]] = currentStudent     
+    return roster
+
+def printStudents(roster):
+    #
+    # Prints the information for each Student object in roster... including studentID,
+    # firstName, lastName, testScores, and average for each object.
+    #
+    # roster A dict of objects from the Student class, each object contains a
+    # studentID(str), firstName(str), lastName(str),
+    # testScores(list of int value), and average (float).
+    #
+    # There is no return value.
+    #
+
+    print("Students in roster:")
+    for student in roster.values():
+        print()
+        print(student)
+
+def updateName(roster, stuID):
+    #
+    # Prompts the user to enter the first name and last name of a student. Then,
+    # uses the setName() instance method to change the name for the Student object
+    # in the dictionary roster with the key = stuID.
+    #
+    # roster A dictionary of objects from the Student class, using stuID as the
+    # key, and a Student object as the value.
+    # stuID A str that is the key for a Student object in the dictionary roster.
+    #
+    # There is no return value.
+    #
+    roster[stuID].setName(input("First name: "), input("Last Name: "))
+    
+
+def updateTests(roster, stuID):
+    #
+    # Prompts the user to enter test scores for a student using addTest() to add
+    # each one to the Student object in the dictionary roster with the key = stuID.
+    #
+    # roster A dictionary of objects from the Student class, using stuID as the
+    # key, and a Student object as the value.
+    # stuID A str that is the key for a Student object in the dictionary roster.
+    #
+    # There is no return value.
+    #
+    currentStudent = roster[stuID]
+    score = input("Enter a test Score (<enter> to stop): ")
+    while score:
+        currentStudent.addTest(int(score))
+        score = input("Enter a test Score (<enter> to stop): ")
+
+
+
+roster = getStudents()
+printStudents(roster)
+
+print()
+print()
+print("You may update any student info, or add a student.")
+
+print()
+currentID = input("Enter Student ID (<enter> to stop): ")
+while currentID:
+    if currentID in roster:
+        print(roster[currentID])
+
+        print()
+        print("(1) Change the Name")
+        print("(2) Add a Test")
+        print("(3) Do Nothing")
+
+        print()
+        choice = input("What would you like to do? ")
+        if choice == "1":
+            updateName(roster, currentID)
+        if choice == "2":
+            updateTests(roster, currentID)
+        
+        print()
+        print(roster[currentID])
+    else:
+        print("This student does not exist. You may enter the info now.")
+        roster[currentID] = Student(currentID)
+        updateName(roster, currentID)
+        updateTests(roster, currentID)
+        
+        print()
+        print("New Student Added:")
+        print(roster[currentID])
+    
+    print()
+    currentID = input("Enter Student ID (<enter> to stop): ")
